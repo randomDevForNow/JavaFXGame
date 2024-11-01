6c05d8be656414323dec1b1b926d67f9eb910ab4
Balongie <butiubalong@gmail.com>
2024-11-02 02:56:12 +0800

    added customized buttons

--- src/main/java/com/sunsofgod/Scenes/RiderMainMenuScene.java
+++ src/main/java/com/sunsofgod/Scenes/RiderMainMenuScene.java
@@ -7,6 +7,7 @@ import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
 import static com.almasb.fxgl.dsl.FXGL.getSceneService;
 import static com.almasb.fxgl.dsl.FXGL.getService;
 
+
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
@@ -14,6 +15,11 @@ import com.almasb.fxgl.dsl.FXGL;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 
 public class RiderMainMenuScene extends FXGLMenu {
 
@@ -23,25 +29,43 @@ public class RiderMainMenuScene extends FXGLMenu {
         VBox vbox = new VBox(10);
         vbox.setAlignment(javafx.geometry.Pos.CENTER);
 
-        // Create buttons
-        Button startButton = new Button("Start New Game");
+        Button startButton = new Button("");
+        startButton.setBackground(Background.EMPTY);
+        Image image = new Image(getClass().getResource("/assets/textures/newgameButton.png").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(250); 
+        imageView.setFitHeight(250); 
+
+        startButton.setGraphic(imageView);
         startButton.setOnAction(e -> {
             getSceneService().pushSubScene(new PlayerSelectScene());
             // getSceneService().pushSubScene(new VideokeScene()); call this when videoke
             // ready
         });
 
-        Button optionsButton = new Button("Options");
-        optionsButton.setOnAction(e -> {
+        Button optionButton = new Button("");
+        optionButton.setBackground(Background.EMPTY);
+        Image optionImage = new Image(getClass().getResource("/assets/textures/optionButton.png").toExternalForm());
+        ImageView optionView = new ImageView(optionImage);
+        optionView.setFitWidth(250); 
+        optionView.setFitHeight(250); 
+        optionButton.setGraphic(optionView);
+        optionButton.setOnAction(e -> {
             System.out.println("CLICKED OPTIONS");
         });
 
-        Button quitButton = new Button("Quit Game");
-        quitButton.setOnAction(e -> {
+        Button exitButton = new Button("");
+        exitButton.setBackground(Background.EMPTY);
+        Image exitImage = new Image(getClass().getResource("/assets/textures/exitButton.png").toExternalForm());
+        ImageView exitView = new ImageView(exitImage);
+        exitView.setFitWidth(250); 
+        exitView.setFitHeight(250); 
+        exitButton.setGraphic(exitView);
+        exitButton.setOnAction(e -> {
             System.exit(0);
         });
 
-        vbox.getChildren().addAll(startButton, optionsButton, quitButton);
+        vbox.getChildren().addAll(startButton, optionButton, exitButton);
 
         getContentRoot().getChildren().add(vbox);
 
